30052720f71ee0e8e1c18a6a3922088be3775709
596493381 <596493381@qq.com>
2016-07-09 11:03:39 +0800

    add guide

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','starter.controller'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -27,12 +27,12 @@ angular.module('starter', ['ionic'])
     .state('guide',{
       url:'/guide',
       templateUrl:'./tpls/guide.html',
-      // controller:'tabController'
+      controller:'guideController'
     })
     .state('tabs',{
       url:'/tab',
       templateUrl:'./tpls/tabs.html',
-      // controller:'tabController'
+      controller:'tabController'
     })
     .state('tabs.home',{
       url:'/home',
---
+++ www/js/controller.js
@@ -0,0 +1,13 @@
+angular.module('starter.controller', [])
+  .controller('tabController', ['$scope', function ($scope) {
+
+  }])
+  .controller('guideController', ['$scope', function ($scope,$urlRouterProvider) {
+    $scope.guideSwpier = new Swiper('.guide',{
+       loop:false
+     });
+     $scope.gotab = function(){
+       $urlRouterProvider.otherwise('/tab');
+     };
+
+  }])
